cf584547cb90e229594435880697be4fcc42fb6f
IftekharAhmedS <ahamedsantonu@gmail.com>
2022-04-29 14:38:55 +0600

    added banner

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import './Home.css';
 
 const Home = () => {
     return (
         <div>
-            
+            <div className="banner-area">
+
+            </div>
         </div>
     );
 };
